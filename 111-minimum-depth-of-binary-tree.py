bd5c282de8d2f193eabc57e12c031982ad728b1b
Jyun-NengJi <jyunnengji@gmail.com>
2019-01-26 11:51:07 +0800

    BFS solution

---
+++ 111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,58 @@
+"""
+Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.
+"""
+import collections
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        queue = collections.deque([])
+        queue.append([root, 1])
+        # BFS
+        while queue:
+            node, depth = queue.popleft()
+            # find the first leaf
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append([node.left, depth + 1])
+            if node.right:
+                queue.append([node.right, depth + 1])
+
+
+if __name__ == "__main__":
+    vals = [3, 9, 20, null, null, 15, 7]
+    root = TreeNode(vals[0])
+    node = root
+    node.left = TreeNode(vals[1])
+    node.right = TreeNode(vals[2])
+    node = node.right
+    node.left = TreeNode(vals[5])
+    node.right = TreeNode(vals[6])
+    print(Solution().minDepth(root))
